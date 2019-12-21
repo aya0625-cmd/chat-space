6391159666b385521a2cf7f08ad0a10952512555
aya0625-cmd <ayaaya06250625@gmail.com>
2019-12-21 20:57:24 +0900

    create_groups2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
+  root "groups#index"
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :edit, :create, :update]
+  resources :groups, only: [:new, :edit, :create, :update] 
+    
+  
 end
