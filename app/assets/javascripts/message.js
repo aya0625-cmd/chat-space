e9101022c36afcdf1f5854dc51d6596005659357
aya0625-cmd <ayaaya06250625@gmail.com>
2020-01-09 21:38:46 +0900

    formatをjson形式に変換

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,64 +1,65 @@
-$(function(){
+$(function(){ 
   function buildHTML(message){
-    if ( message.image ) {
-      var html =
-       `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-          <img src=${message.image} >
-        </div>`
+   if( message.image) {
+     var html =
+      `<div class="chat-main__message" data-message-id=${message.id}>
+         <div class="chat-main__message__data">
+           <p class="chat-main__message__data__name">
+             ${message.user_name}
+           </p>
+           <p class="chat-main__message__data__date">
+             ${message.created_at}
+           </p>
+         </div>
+         <div class="chat-main__message">
+           <p class="chat-main__message__text">
+             ${message.text}
+           </p>
+           <img class= "chat-main__message__image" src= ${message.image} >
+         </div>
+       </div>`
       return html;
-    } else {
-      var html =
-       `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-        </div>`
-      return html;
-    };
+   } else {
+     var html =
+     `<div class="chat-main__message" data-message-id=${message.id}>
+        <div class="chat-main__message__data">
+          <p class="chat-main__message__data__name">
+            ${message.user_name}
+          </p>
+          <p class="chat-main__message__data__date">
+            ${message.created_at}
+          </p>
+        </div>
+        <div class="chat-main__message">
+          <p class="chat-main__message__text">
+            ${message.text}
+          </p>
+        </div>
+      </div>`
+     return html;
+  };
   }
-  $('#message_text').on('submit', function(e){
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: './messages.json',
-      type: 'POST',
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-     .done(function(data){
-       var html = buildHTML(data);
-       $('.messages').append(html);      
-       $('form')[0].reset();
-       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-     })
-     .fail(function() {
-       alert("メッセージ送信に失敗しました");
-     });
-  })
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+      $.ajax({
+        url: url,
+        type: 'POST',
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data){  
+        var html = buildHTML(data);
+        $('.chat-main__center').append(html); 
+        $('.chat-main__center').animate({ scrollTop: $('.chat-main__center')[0].scrollHeight});   
+        $(".submit-btn").prop("disabled", false);  
+        $('form')[0].reset()
+      })
+      .fail(function(){
+        alert('メッセージ送信に失敗しました');
+      });
+  });
 });
