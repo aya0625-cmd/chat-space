f165c7f223bf01e4b3b7a9a007031cd48ef5faab
aya0625-cmd <ayaaya06250625@gmail.com>
2020-02-01 14:48:32 +0900

    setting-S3

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,8 +26,15 @@ set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 set :linked_files, %w{ config/secrets.yml }
 
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
+
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
@@ -44,4 +51,5 @@ namespace :deploy do
   end
   before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
+
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,12 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
+ 
+  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
+    config.storage = :file
+  elsif Rails.env.production? #本番はS3に保存する
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
@@ -13,5 +17,7 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'aya-chatspace'
-  config.asset_host = 'https://aya-chat-space.s3.amazonaws.com'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/aya-chatspace'
+
+
 end
