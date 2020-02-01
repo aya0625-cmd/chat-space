4d5a12357d744ba1bdbba6ec759cb11957a1f716
aya0625-cmd <ayaaya06250625@gmail.com>
2020-02-01 15:08:22 +0900

    setting-S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,10 +3,6 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
- 
-  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
-    config.storage = :file
-  elsif Rails.env.production? #本番はS3に保存する
     config.storage = :fog
     config.fog_provider = 'fog/aws'
   config.fog_credentials = {
@@ -15,9 +11,6 @@ CarrierWave.configure do |config|
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
   }
-
   config.fog_directory  = 'aya-chatspace'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/aya-chatspace'
-
-
 end
